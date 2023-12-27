ef4a59eb479b516752857ac73246d4d2f1f73598
jason <jasonwchristiansen@gmail.com>
2023-12-27 15:39:51 -0600

    add argparse

--- src/gmail_search_extractor.py
+++ src/gmail_search_extractor.py
@@ -1,9 +1,12 @@
 # %%
 # Imports #
 
+import sys
 import os
 from os.path import expanduser
+import argparse
 import pandas as pd
+import datetime
 
 from config import (
     file_dir,
@@ -24,16 +27,70 @@ from utils.display_tools import print_logger, pprint_df, pprint_ls
 # %%
 # Variables #
 
-search_string = "Maggie@romancetravelgroup.com"
+today_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-df = get_body_dataframe_from_search_string(
-    search_string,
-    auth_dir=os.path.join(expanduser("~"), "credentials", "personal", "gmail_auth"),
-)
 
-pprint_df(df)
+# %%
+# Main #
 
-# save to csv
-df.to_csv(os.path.join(report_dir, "email_bodies.csv"), index=False)
+
+if __name__ == "__main__":
+    search_string = f"from:me to:me after:{today_date}"
+    authentication_directory = os.path.join(
+        expanduser("~"), "credentials", "personal", "gmail_auth"
+    )
+
+    if "ipykernel" in sys.argv[0]:
+        print("Running in IPython kernel")
+    else:
+        parser = argparse.ArgumentParser(description="Export gmail search as csv")
+        # search string
+        parser.add_argument(
+            "-s", "--search-string", type=str, help="String to search gmail for"
+        )
+        # outfile
+        parser.add_argument("-o", "--outfile", type=str, help="File to save output to")
+        # authentication_directory
+        parser.add_argument(
+            "-a",
+            "--authentication-directory",
+            type=str,
+            help="Directory to store authentication files",
+        )
+
+        args = parser.parse_args()
+
+        if args.search_string:
+            search_string = args.search_string
+        else:
+            print(f"No search string specified. Using default: {search_string}")
+
+        if args.outfile:
+            outfile = args.outfile
+        else:
+            outfile = os.path.join(report_dir, "email_bodies.csv")
+            print(f"No outfile specified. Using default: {outfile}")
+
+        if args.authentication_directory:
+            authentication_directory = args.authentication_directory
+        else:
+            print(
+                f"No authentication directory specified. Using default: {authentication_directory}"
+            )
+
+    print_logger(f"Searching gmail for: {search_string}")
+
+    df = get_body_dataframe_from_search_string(
+        search_string,
+        auth_dir=authentication_directory,
+    )
+
+    print_logger("Output (first 20 rows):")
+    pprint_df(df.head(20))
+
+    print_logger(f"Saving to: {outfile}")
+    df.to_csv(outfile, index=False)
+
+    print_logger("Done")
 
 # %%
