ec65a2305eeeb02f17ae2766047507dbaf5cc64d
jason <jasonwchristiansen@gmail.com>
2025-04-23 20:41:23 -0500

    sync config scripts

--- src/config.py
+++ src/config.py
@@ -6,32 +6,27 @@ import sys
 from os.path import expanduser
 
 home_dir = expanduser("~")
-
 file_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 grandparent_dir = os.path.dirname(parent_dir)
 great_grandparent_dir = os.path.dirname(grandparent_dir)
 
 data_dir = os.path.join(parent_dir, "data")
-trigger_dir = os.path.join(parent_dir, "triggers")
+templates_dir = os.path.join(parent_dir, "templates")
 log_dir = os.path.join(parent_dir, "logs")
 src_dir = os.path.join(parent_dir, "src")
 src_utils_dir = os.path.join(src_dir, "utils")
 drive_download_cache_dir = os.path.join(data_dir, "drive_download_cache")
-
-report_dir = os.path.join(parent_dir, "reports")
-email_attachment_dir = os.path.join(data_dir, "email_attachments")
-
+s3_download_cache = os.path.join(data_dir, "s3_download_cache")
 
 directories = [
     data_dir,
-    trigger_dir,
+    templates_dir,
     log_dir,
     src_dir,
     src_utils_dir,
     drive_download_cache_dir,
-    report_dir,
-    email_attachment_dir,
+    s3_download_cache,
 ]
 for directory in directories:
     if not os.path.exists(directory):
@@ -45,6 +40,7 @@ sys.path.append(src_dir)
 sys.path.append(src_utils_dir)
 
 if __name__ == "__main__":
+    print(f"home_dir: {home_dir}")
     print(f"file_dir: {file_dir}")
     print(f"parent_dir: {parent_dir}")
     print(f"grandparent_dir: {grandparent_dir}")
